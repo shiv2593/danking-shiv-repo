d4307af78698dd93eb9bbcdc8ad5b9bdc17938ed
shiv2593 <115607185+shiv2593@users.noreply.github.com>
2024-07-19 11:40:55 +0530

    Update Terraform.tf

--- Terraform.tf
+++ Terraform.tf
@@ -125,7 +125,7 @@ resource "aws_eip" "proj-eip" {
 
 
 # Creating an ubuntu EC2 instance
-resource "aws_instance" "Prod-Server" {
+resource "aws_instance" "Test-Server" {
  ami = "ami-0a0e5d9c7acc336f1"
  instance_type = "t2.micro"
  availability_zone = "us-east-1a"
